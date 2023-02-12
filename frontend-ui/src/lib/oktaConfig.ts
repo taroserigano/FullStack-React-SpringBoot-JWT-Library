9690ba09513b36b526b73ba7f3e28fcb680a73c8
Taro Serigano <taroserigano@gmail.com>
2023-02-12 15:02:53 -0600

    Update oktaConfig.ts

--- frontend-ui/src/lib/oktaConfig.ts
+++ frontend-ui/src/lib/oktaConfig.ts
@@ -1,6 +1,6 @@
 export const oktaConfig = {
-    clientId: '0oa7mzk48v0l4S6Vj5d7',
-    issuer: 'https://dev-29252623.okta.com/oauth2/default',
+    clientId: '<ID>',
+    issuer: 'https://<ID>/oauth2/default',
     redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
